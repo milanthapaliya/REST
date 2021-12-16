1a4beb1dad10fe2ad0b675ac2d92bdc82f19558d
Milan Thapaliya <thapaliyamilan8@gmail.com>
2021-12-16 21:14:44 +0545

    POST operation successfully completed

--- app.js
+++ app.js
@@ -9,8 +9,9 @@
 const express = require('express')
 const app = express()
 require('./db')
+const Student = require('./models/student')
 app.set('view engine', 'ejs')
-
+app.use(express.urlencoded({extended : false}))
 app.get('/',(req,res) =>{
     res.render('index', {title : 'Home'})
 })
@@ -20,6 +21,28 @@ app.get('/about', (req,res) => {
 app.get('/add', (req, res) => {
     res.render('addstudent', {title : 'Add Students'})
 })
+app.post('/students', (req, res) => {
+    console.log(req.body)
+    const student = new Student({
+        name : req.body.name,
+        age : req.body.age
+    })
+    student.save().then(() => {
+        console.log("Data Insert Success");
+        res.send("Data successfully inserted")
+    }).catch((e) =>{
+        console.log(e);
+        res.send("Data Insert Failed");
+    })
+})
+
+// app.get('students', (req,res) => {
+//     const student = new Student({
+//         name : 'Milan',
+//         age : 23
+//     })
+//     student.save().then(() => console.log("Insert Success")).catch((e) => console.log(e))
+// })
 
 //404 page using middleware
 app.use((req,res) => {
--- models/student.js
+++ models/student.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose')
-const students = new mongoose()
+const studentSchema = mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    age : {
+        type : Number,
+        required : true
+    }
+}, {timestamps : true});
+
+const Student = mongoose.model('Student', studentSchema);
+module.exports = Student;
